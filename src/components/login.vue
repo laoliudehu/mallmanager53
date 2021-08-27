7cf45fd31703a3c1cbcd35038c07ee98dbb34e73
laoliudehu <l1010256157@163.com>
2021-08-27 22:38:24 +0800

    新建登录分支、组件配置路由

---
+++ src/components/login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>denwe</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      nmae: 'login',
+      path: '/login',
+      component: Login
      
     }
   ]
