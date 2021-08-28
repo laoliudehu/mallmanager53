a68711d08c617967c81a82e0b5d377dfae9a4a5a
laoliudehu <l1010256157@163.com>
2021-08-28 08:12:18 +0800

    新建

---
+++ src/components/login/login.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-form :label-position="top" label-width="80px" :model="formdata">
+  <el-form-item label="用户">
+    <el-input v-model="formdata.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formdata.password"></el-input>
+  </el-form-item>
+  
+ 
+    </el-form>
+</template> 
+
+ <script>
+export default {
+    data(){
+        return {
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    }
+}
+</script>  
+
+<style>
+    
+</style>
+
+<!-- <template>
+    <div>sdsa</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style> -->
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import '@/assets/css/reset.css'
 
 Vue.use(ElementUI);
 
